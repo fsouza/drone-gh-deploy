45af8b9c2b9b778804eaebf3c09be18db0deaeb6
Francisco Souza <francisco.souza@nytimes.com>
2019-02-14 12:59:46 -0500

    no check

--- ci/trigger-deploy.sh
+++ ci/trigger-deploy.sh
@@ -18,29 +18,20 @@ function get_repo_data() {
         -F pullRequest=${DRONE_PULL_REQUEST:-0}
 }
 
-set +e
-node /usr/src/check-for-pr/index.js
-status=$?
-set -e
+repo_data="$(get_repo_data)"
+repo_id=$(echo "$repo_data" | jq -r .data.repository.id)
+ref_id=$(echo "$repo_data" | jq -r 'if .data.repository.ref then .data.repository.ref.id else .data.repository.pullRequest.headRef.id end')
 
-if [ "$status" = "0" ]; then
-    repo_data="$(get_repo_data)"
-    repo_id=$(echo "$repo_data" | jq -r .data.repository.id)
-    ref_id=$(echo "$repo_data" | jq -r 'if .data.repository.ref then .data.repository.ref.id else .data.repository.pullRequest.headRef.id end')
-
-    auto_merge=true
-    if [ "$environment" = "production" ]; then
-        auto_merge=false
-    fi
-
-    hub api graphql \
-        -H "Accept: application/vnd.github.flash-preview+json" \
-        -F query=@${SCRIPT_DIR}/queries/create-deployment.graphql \
-        -F environment=$environment \
-        -F description="${DRONE_COMMIT_MESSAGE}" \
-        -F refId=$ref_id \
-        -F repositoryId=$repo_id \
-	-F autoMerge=$auto_merge
-else
-    echo "won't trigger deployment"
+auto_merge=true
+if [ "$environment" = "production" ]; then
+    auto_merge=false
 fi
+
+hub api graphql \
+    -H "Accept: application/vnd.github.flash-preview+json" \
+    -F query=@${SCRIPT_DIR}/queries/create-deployment.graphql \
+    -F environment=$environment \
+    -F description="${DRONE_COMMIT_MESSAGE}" \
+    -F refId=$ref_id \
+    -F repositoryId=$repo_id \
+    -F autoMerge=$auto_merge
